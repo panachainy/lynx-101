3b817fe86766ab3b8cd5cd835464d121605a194d
Panachai <panachai.ny@gmail.com>
2025-03-23 13:15:22 +0700

    Add Example route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { MemoryRouter, Route, Routes } from 'react-router'
 import { Home } from './Home'
+import { Example } from './Example'
 
 export function App() {
   return (
     <MemoryRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/example" element={<Example />} />
       </Routes>
     </MemoryRouter>
   )
